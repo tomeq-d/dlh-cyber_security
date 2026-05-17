6b6ce0c3e0f08c864ef069df24482fbade96ffa6
tomeq-d <tdembowski@gmail.com>
2026-05-17 01:39:12 -0700

    Task 4: using corrected script

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-john --wordlist=usr/share/wordlist/rockyou.txt "$1" | john --show "$1" | awk -F: '{print $2}' > 4-password.txt
-
+( john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" ) | awk -F: '{print $2}' > 4-password.txt
