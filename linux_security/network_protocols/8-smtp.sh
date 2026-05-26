d93d16da3b8633dd1dae4bce53db6b03064a4c6e
tomeq-d <tdembowski@gmail.com>
2026-05-25 23:59:34 -0700

    Fix Task 8 - examine SMTP server settings

--- linux_security/network_protocols/8-smtp.sh
+++ linux_security/network_protocols/8-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo grep "smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
+sudo grep "^smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
