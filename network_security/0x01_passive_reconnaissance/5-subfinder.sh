19aae935acdf7bb7a49297aad1e246836b6d4d79
tomeq-d <tdembowski@gmail.com>
2026-05-27 01:57:33 -0700

    Fix Task 5 - Find the sub

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | awk -v d="$1" '{print; cmd="dig +short " $1 " | tail -n1"; cmd | getline ip; close(cmd); if(ip!="") print $1 "," ip >> d".txt"}'
+subfinder -d $1 -silent -o $1.txt -nW -oI
