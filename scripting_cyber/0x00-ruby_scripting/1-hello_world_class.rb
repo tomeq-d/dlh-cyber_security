4e462d02e64f4f18d461f7859beae0803ab2c404
tomeq-d <tdembowski@gmail.com>
2026-09-17 17:34:52 +0200

    Add Task 1: Hello World wtih Class

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# HelloWorld - stores and displays a greeting message
+class HelloWorld
+  # initialize - sets the default greeting message
+  def initialize
+    @message = 'Hello, World!'
+  end
+
+  # print_hello - displays the greeting message
+  def print_hello
+    puts @message
+  end
+end
