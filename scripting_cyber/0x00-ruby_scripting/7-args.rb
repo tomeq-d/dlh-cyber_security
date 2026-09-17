57f195d2966464e9761d5c1f5047f274604e5069
tomeq-d <tdembowski@gmail.com>
2026-09-17 17:55:31 +0200

    Add Task 7: Command-Line Arguments

---
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# print_arguments - prints each command-line argument passed to the
+# script, numbered from 1, or a message if none were provided
+def print_arguments
+  if ARGV.empty?
+    puts 'No arguments provided.'
+  else
+    ARGV.each_with_index { |arg, index| puts "#{index + 1}. #{arg}" }
+  end
+end
