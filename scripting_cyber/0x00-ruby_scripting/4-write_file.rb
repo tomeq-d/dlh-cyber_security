604afa670d7254510fd3b7a04fed5d5f37976823
tomeq-d <tdembowski@gmail.com>
2026-09-17 17:47:47 +0200

    Add Task 4: Writing to a File

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -12,4 +12,5 @@ def merge_json_files(file1_path, file2_path)
   destination = JSON.parse(File.read(file2_path))
   merged = destination + source
   File.write(file2_path, JSON.pretty_generate(merged) + "\n")
+  puts "Merged JSON written to #{file2_path}"
 end
