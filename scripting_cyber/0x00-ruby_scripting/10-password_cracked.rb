7c6cd831888fa27f5ab58d4afc6559f65f78946d
tomeq-d <tdembowski@gmail.com>
2026-09-18 14:32:07 +0200

    Add Task 10: Password Cracker

---
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'digest'
+
+if ARGV.length < 2
+  puts 'Usage: 10-password_cracked.rb HASHED_PASSWORD DICTIONARY_FILE'
+  exit
+end
+
+hashed_password = ARGV[0]
+dictionary_file = ARGV[1]
+
+found = false
+
+File.foreach(dictionary_file) do |line|
+  word = line.strip
+  next if word.empty?
+
+  if Digest::SHA256.hexdigest(word) == hashed_password
+    puts "Password found: #{word}"
+    found = true
+    break
+  end
+end
+
+puts 'Password not found in dictionary.' unless found
